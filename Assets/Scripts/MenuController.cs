e0dff8cb73ad620bc2313e52d55781895b5ae132
minikewo <anja.kovacik@gmail.com>
2025-05-29 15:52:11 +0200

    Changed Menu Access
    changed so the menu is now accessible by pressing "Tab" instead of "Esc"

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -12,7 +12,7 @@ public class MenuController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
             menuCanvas.SetActive(!menuCanvas.activeSelf);
         }
